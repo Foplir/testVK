1ce52fd6c4b8be70c3191b0b11d311b327c03fc0
Foplir <104756080+Foplir@users.noreply.github.com>
2024-05-23 16:44:26 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,18 @@
+import telebot
+import ollama
+
+MODEL = 'phi3'
+
+token='7117200365:AAFkFjzxT6k_Fd7ZvuhjpcpaLidsl42MpvA'
+bot=telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id,'Привет')
+
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    messagee = {'role':'user', 'content': message.text}
+    response = ollama.chat(model=MODEL, messages=[message])
+    bot.send_message(message.chat.id, response['message']['content'])
+    
+bot.infinity_polling()
